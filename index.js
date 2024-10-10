f24bbec3deeb1bcac353230a5ced8b8c0b781aa7
Apple <apple@Apples-MacBook-Pro.local>
2024-10-10 11:09:45 +0700

    Add Bot Tele

--- index.js
+++ index.js
@@ -22,14 +22,19 @@ app.post('/qr/save/transaction', (req, res) => {
 
     //     })
     // })
-//   var price = req.body.price
-//     var user_id =  req.body.user_id
-//     var chat_id='693726757';
-//     var token='6088240244:AAH3ZkMDYRanrJ-FPu29FNCVe7LZLZb2A1c';
-//     var text= "siengroeun2018"
+    var price = req.body.price
+    var user_id =  req.body.user_id
+    var md5 =req.body.md5
+    // var chat_id='693726757';
+    // var token='6088240244:AAH3ZkMDYRanrJ-FPu29FNCVe7LZLZb2A1c';
+    // var text= "siengroeun2018"
 //     const TelegramBot = require('node-telegram-bot-api');
 //     const bot = new TelegramBot(token, {polling: true});
 //     bot.sendMessage(chat_id, 'Hello Paid KHQR From User ID  : ' +user_id+ ' Price ' + price +' Hash '+req.body.md5 );
+    // var price = 1
+    // var user_id = 1
+    // var md5 ="asdasd"
+    Axios.get('https://api.camfeed.net/sendbot/'+price+'/'+user_id+'/'+md5,)
     res.json({
         msg: "Transaction has been saved",
         code: 0
